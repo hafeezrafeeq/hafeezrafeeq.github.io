8f0c90a387d7f5d37da55f2228569f8b7ed7b765
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2026-02-24 13:31:34 +0500

    feat: initialize React project with Vite
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in the public directory.
    - Created App.css for basic styling.
    - Developed App component with Navbar integration.
    - Added React logo SVG in assets.
    - Implemented Navbar component with a placeholder message.
    - Set up main entry point for React application.
    - Configured Vite with React plugin in vite.config.js.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './componets/Navbar.jsx'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import React from "react"
+
+const Navbar = () => {
+
+  return (
+    <>
+   
+      <div>helo wold</div>
+      
+    </>
+  )
+}
+
+export default Navbar
