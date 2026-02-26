9a68172163e97083193b05cfdb21a10df3207966
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2026-02-26 23:45:17 +0500

    feat: integrate Tailwind CSS and update Navbar component
    - Added Tailwind CSS and its Vite plugin to dependencies and configuration.
    - Updated App.css to include Tailwind CSS imports and reset styles.
    - Refactored Navbar component to use Tailwind CSS for styling and layout.

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -5,9 +5,21 @@ const Navbar = () => {
 
   return (
     <>
-   
-      <div>helo wold</div>
-      
+      <nav className="flex justify-around ">
+        <div className="logo">logo</div>
+
+        <div className="flex gap-6">
+          <a href="Home">Home</a>
+          <a href="About">About</a>
+          <a href="Service">Service</a>
+          <a href="Contect">Contect</a>
+        </div>
+
+        <div className="btn-contect">Contect Now</div>
+
+      </nav>
+
+
     </>
   )
 }
