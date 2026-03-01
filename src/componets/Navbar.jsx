10366c79c4094988eb26273ed5958a44485d24bb
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2026-03-01 23:36:50 +0500

    feat: refactor Navbar component to use motion for animated links and improve structure

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -11,24 +11,37 @@ const Navbar = () => {
       <nav className="flex justify-between items-center font-[f-regular] py-4 px-14 border-gray-300 border-b-[0.2px] ">
         <div className="logo ">logo</div>
 
-        <div className="flex gap-12">
+        <motion.div
+
+
+          className="flex gap-12">
           {[{ name: "Home", Link: "#" },
           { name: "About", Link: "#about" },
           { name: "Service", Link: "#home" },
           { name: "Contect", Link: "#home" }
-          ].map((item,index)=>(
+          ].map((item, index) => (
             <motion.a
-            key={index}
-            href={item.link}
-            className="border-b-2"
+              initial="rest"
+              whileHover="hovered"
+              animate="rest"
+              key={index}
+              href={item.Link}
+              className="relative text-gray-600 hover:text-gray-950"
             >
               {item.name}
+              <motion.span
+                variants={{
+                  rest: { width: 0 },
+                  hovered: { width: "100%" },
+                }}
+                className="absolute left-0 bottom-0  h-[2px] bg-[#c9ee7d]"
+              />
             </motion.a>
           ))}
-          
-        </div>
 
-       
+        </motion.div>
+
+
 
         <button className="btn-contect flex justify-between items-center">
           Contect Now
